2b2b02827785ec1a04a3c4d43026d892e1f6ae45
kpat5 <karanpatrick233@gmail.com>
2022-11-13 20:12:35 +0530

    initial

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+
 module.exports = {
+  networks:{
+    hardhat:{
+      chainId:1337
+    },
+    mumbai:{
+      url:"https://polygon-mumbai.g.alchemy.com/v2/"
+    },
+    mainnet:{}
+  }
   solidity: "0.8.17",
 };
