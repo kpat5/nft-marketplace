50ecc11ccccfc2db5740144d9dccd38b821dcd02
kpat5 <karanpatrick233@gmail.com>
2022-11-14 22:44:54 +0530

    nft contract

---
+++ contracts/NFTMarket.sol
@@ -0,0 +1,11 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+contract NFTMarket is ReentrancyGuard{
+    using Counters for Counters.Counter;
+    
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+const fs = require("fs");
 /** @type import('hardhat/config').HardhatUserConfig */
-
+const projectId = "GLh1eMWcEbaOr-CnyyHf3jRjEKsqeCee";
+const privateKey = fs.readFileSync(".secret").toString();
 
 module.exports = {
-  networks:{
-    hardhat:{
-      chainId:1337
+  networks: {
+    hardhat: {
+      chainId: 1337,
     },
-    mumbai:{
-      url:"https://polygon-mumbai.g.alchemy.com/v2/"
+    mumbai: {
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${projectId}`,
+      accounts: [privateKey],
     },
-    mainnet:{}
-  }
+    mainnet: {},
+  },
   solidity: "0.8.17",
 };
